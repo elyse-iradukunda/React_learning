0f324c8ceed2ed739679ff87623e0b5fff8af362
Your Name <ellycreativity8@gmail.com>
2026-02-11 19:54:29 +0200

    calculator app

--- Calculator_App/src/App.jsx
+++ Calculator_App/src/App.jsx
@@ -1,57 +1,57 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react"
 
+ function App() {
+  const [input, setInput] = useState("")
 
-function App() {
+  function handleClick(value) {
 
-  return(
+        setInput(input + value);
+  }
+
+  function clearInput() {
+    setInput("");
+  }
+
+  function calculate() {
+    try {
+      setInput(eval(input).toString())
+    } catch {
+      setInput("Hi bro Enter something better")
+    }
+  }
+
+  return (
     <>
+ 
+    <div className="calculator">
+      <h2>Elyse Calculator</h2>
+      <input type="text" value={input}  />
 
-<nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      <div className="buttons">
+           <button onClick={() => handleClick("7")}>7</button>
+        <button onClick={() => handleClick("8")}>8</button>
+            <button onClick={() => handleClick("9")}>9</button>
+        <button onClick={() => handleClick("/")}>/</button>
+
+        <button onClick={() => handleClick("4")}>4</button>
+           <button onClick={() => handleClick("5")}>5</button>
+        <button onClick={() => handleClick("6")}>6</button>
+         <button onClick={() => handleClick("*")}>*</button>
+
+        <button onClick={() => handleClick("1")}>1</button>
+        <button onClick={() => handleClick("2")}>2</button>
+        <button onClick={() => handleClick("3")}>3</button>
+        <button onClick={() => handleClick("-")}>-</button>
+        <button onClick={() => handleClick("0")}>0</button>
+        <button onClick={clearInput}>C</button>
+        <button onClick={calculate}>=</button>
+        <button onClick={() => handleClick("+")}>+</button> 
+      </div>
     </div>
-  </div>
-</nav>
 
-<img src="./src/assets/image.png" className="img-fluid w-20/3 h-20" alt="..."></img>
-         
-
-    <button className="btn btn-primary">Submit</button>
-    <h1>Hi</h1>    
-</>
+   </> 
   )
-  
-  
 }
+
 
 export default App
