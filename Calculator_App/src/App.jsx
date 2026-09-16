267cd1c8873b48dc9eb7e7b1ee7dfeba8c75559c
Your Name <ellycreativity8@gmail.com>
2026-09-16 23:14:59 +0200

    backup

--- Calculator_App/src/App.jsx
+++ Calculator_App/src/App.jsx
@@ -1,57 +1,13 @@
-import { useState } from "react"
 
  function App() {
-  const [input, setInput] = useState("")
-
-  function handleClick(value) {
-
-        setInput(input + value);
-  }
-
-  function clearInput() {
-    setInput("");
-  }
-
-  function calculate() {
-    try {
-      setInput(eval(input).toString())
-    } catch {
-      setInput("Hi bro Enter something better")
-    }
-  }
-
   return (
-    <>
- 
-    <div className="calculator">
-      <h2>Elyse Calculator</h2>
-      <input type="text" value={input}  />
+    <div>
 
-      <div className="buttons">
-           <button onClick={() => handleClick("7")}>7</button>
-        <button onClick={() => handleClick("8")}>8</button>
-            <button onClick={() => handleClick("9")}>9</button>
-        <button onClick={() => handleClick("/")}>/</button>
-
-        <button onClick={() => handleClick("4")}>4</button>
-           <button onClick={() => handleClick("5")}>5</button>
-        <button onClick={() => handleClick("6")}>6</button>
-         <button onClick={() => handleClick("*")}>*</button>
-
-        <button onClick={() => handleClick("1")}>1</button>
-        <button onClick={() => handleClick("2")}>2</button>
-        <button onClick={() => handleClick("3")}>3</button>
-        <button onClick={() => handleClick("-")}>-</button>
-        <button onClick={() => handleClick("0")}>0</button>
-        <button onClick={clearInput}>C</button>
-        <button onClick={calculate}>=</button>
-        <button onClick={() => handleClick("+")}>+</button> 
-      </div>
+          <div className="text-amber-300">
+            <p>Box</p>
+          </div>      
     </div>
-
-   </> 
-  )
-}
-
-
-export default App
+  );
+ }
+ 
+ export default App;
---
+++ Calculator_App/tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2563EB",
+        accent: "#F97316",
+        "dark-indigo": "#3730A3",
+      },
+    },
+  },
+  plugins: [],
+}
